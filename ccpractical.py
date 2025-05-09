290b4b3818bce16fe3cd550c78acf312e81c9e26
atharva-2454 <atharvasuryawanshi98@gmail.com>
2025-05-09 10:39:41 +0530

    Update ccpractical.py

--- ccpractical.py
+++ ccpractical.py
@@ -1,5 +1,2 @@
 print("CC Practical Examination ")
-list = [1,2,3,4,5,]
-print(list)
-practical=[Install and configure google app engine ]
-print(practical)
+print("Install and configure google app engine")
